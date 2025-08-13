872719748ca002610e328332b0d82e842c7b784c
mhuwiler <marc.huwiler@cern.ch>
2025-08-13 14:19:56 +0200

    Changing the setup for 2024 scouting nano samples

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@
 # Author(s): Raghav Kansal
 #############################################################
 
-CMSSW_VER=CMSSW_14_0_6_patch1
+CMSSW_VER=CMSSW_14_2_2
 this_file="$( [ ! -z "$ZSH_VERSION" ] && echo "${(%):-%x}" || echo "${BASH_SOURCE[0]}" )"
 this_dir="$( cd "$( dirname "$this_file" )" && pwd )"
 
@@ -34,17 +34,7 @@ if ! [ -f "$this_dir/cmssw/$CMSSW_VER/.installed" ]; then
     run_cmd cd $CMSSW_VER/src
     run_cmd eval `scramv1 runtime -sh`
 
-    #install CMSSW updates not in official releases
-    master_ver=`echo $CMSSW_VER | cut -d'_' -f 2`
-    sub_ver=`echo $CMSSW_VER | cut -d'_' -f 3`
-    subsub_ver=`echo $CMSSW_VER | cut -d'_' -f 4`
-    if [[ $master_ver == "14"  && $sub_ver == "0" ]]; then
-      run_cmd echo "=> Installing addons for CMSSW_"${master_ver}"_"$sub_ver
-      run_cmd git cms-checkout-topic DAZSLE:CMSSW_14_0_6_BTVnano_GPT
-      # Extra tau models from Tau POG
-      run_cmd wget https://github.com/cms-tau-pog/RecoTauTag-TrainingFiles/raw/refs/heads/BoostedDeepTau_v2/BoostedDeepTauId/boosteddeepTau_RunIIv2p0_{core,inner,outer}.pb -P RecoTauTag/TrainingFiles/data/BoostedDeepTauId/
-      run_cmd wget https://github.com/cms-tau-pog/RecoTauTag-TrainingFiles/raw/refs/heads/deepTau_v2p5_noDomainAdaptation/DeepTauId/deepTau_2018v2p5_noDomainAdaptation_{core,inner,outer}.pb -P RecoTauTag/TrainingFiles/data/DeepTauId/
-    fi
+    # no need to install patches
 
     # custom code
     # not sure why, but this directory structure is necessary...
