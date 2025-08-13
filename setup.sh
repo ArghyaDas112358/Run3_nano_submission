66b107f32aec23b63ed02b14c1bae8200ff2c55b
mhuwiler <marc.huwiler@cern.ch>
2025-08-13 18:38:41 +0200

    Updating setup for NanoAOD v15 production

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@
 # Author(s): Raghav Kansal
 #############################################################
 
-CMSSW_VER=CMSSW_14_2_2
+CMSSW_VER=CMSSW_15_1_0_pre5
 this_file="$( [ ! -z "$ZSH_VERSION" ] && echo "${(%):-%x}" || echo "${BASH_SOURCE[0]}" )"
 this_dir="$( cd "$( dirname "$this_file" )" && pwd )"
 
