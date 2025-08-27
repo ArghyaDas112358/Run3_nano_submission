236d30d2d06842c84429ca23082333fe1e03eee8
mhuwiler <marc.huwiler@cern.ch>
2025-08-27 22:02:20 +0200

    Accepting terminal input to script

--- DAS2json.py
+++ DAS2json.py
@@ -2,6 +2,7 @@
 from __future__ import division, print_function
 
 import os
+import sys
 import copy
 import json
 from argparse import ArgumentParser
@@ -26,6 +27,18 @@ def WriteDatasetDictToJson(result, filename):
 			outfile.write("}")
 			#json.dump(results, outfile, ensure_ascii=False, sort_keys=False) #encoding="utf8", 
 
+def FormatFromCmdInput(cmdinput): 
+	result = ""
+
+	print(cmdinput)
+	cmdinput = cmdinput.rstrip("\n")
+	for line in cmdinput.split("\n"): 
+			key = line.split("/")[1] # the dataset begins with a "/" so need the key after that
+			result += "\"{}\": \"{}\",\n".format(key, line)
+			print("\"{}\": \"{}\",\n".format(key, line))
+
+	return result
+
 
 if __name__ == "__main__":
 
@@ -35,16 +48,22 @@ if __name__ == "__main__":
 	parser.add_argument("-o", "--output", dest="output", action="store", type=str, default="", help="Name of json file for storing output. ")
 	parser.add_argument("--debug", dest="debug", action="store_true", default=False, help="Turn on debug output")
 	parser.add_argument('-b', "--batch", dest="batch", action="store_true", default=False, help="Run in batch mode")
-	
-	args = parser.parse_args()
 
 
-	result = DatasetDictFromFile(args.file)
+	# Reading input in case of command line usage 
+	if not os.isatty(0):
+		cmdinput = sys.stdin.read()
+		datasets = FormatFromCmdInput(cmdinput)
+		print(datasets)
+
+	else: 
+		args = parser.parse_args()
+		datasets = DatasetDictFromFile(args.file)
 
 
-	if(args.show): print(result)
+		if(args.show): print(datasets)
 
-	if (args.output != ""): 
-		WriteDatasetDictToJson(result, args.output)
+		if (args.output != ""): 
+			WriteDatasetDictToJson(datasets, args.output)
 			
 
